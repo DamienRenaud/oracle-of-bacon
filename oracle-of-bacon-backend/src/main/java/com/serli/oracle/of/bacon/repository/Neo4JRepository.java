2edc2094d7d160fe41a4e6e2159d0eb15fa3ff70
Damien Renaud <renauddamien86@gmail.com>
2018-02-20 12:00:12 +0100

    :sparkles: Implementation de la fonction de recherche du chemin le plus court entre un acteur et Kevin Bacon

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
@@ -1,25 +1,71 @@
 package com.serli.oracle.of.bacon.repository;
 
 
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.*;
+import org.neo4j.driver.v1.types.Node;
+import org.neo4j.driver.v1.types.Path;
 
-import java.util.List;
+import java.util.*;
 
 public class Neo4JRepository {
     private final Driver driver;
 
     public Neo4JRepository() {
-        this.driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+        this.driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "damneo4j"));
     }
 
     public List<?> getConnectionsToKevinBacon(String actorName) {
         Session session = driver.session();
 
-        // TODO implement Oracle of Bacon
-        return null;
+        Transaction t = session.beginTransaction();
+
+        // MATCH p=shortestPath((bacon:Actors {name:"Bacon, Kevin (I)"})-[*]-(act:Actors {name:"Backhouse, Ian"})) RETURN p
+        StatementResult sr = t.run("MATCH p=shortestPath((bacon:Actors {name:\"Bacon, Kevin (I)\"})-[*]-(act:Actors {name: \"" + actorName + "\"})) RETURN p");
+
+        ArrayList<Map<String, GraphItem>> ll = new ArrayList<>();
+
+        while (sr.hasNext()) {
+            Record rec = sr.next();
+            Path path = (Path) rec.asMap().get("p");
+
+            // On crée pour chaque noeud un GraphNode
+            path.nodes().forEach(node -> {
+                long id;
+                String type, value;
+                GraphNode graphNode;
+
+                id = node.id();
+
+                if(node.containsKey("name")) {
+                    type = "Actor";
+                    value = node.get("name").asString();
+                    graphNode = new GraphNode(id, value, type);
+                    HashMap<String, GraphItem> map = new HashMap();
+                    map.put("data", graphNode); // On package dans un élément "data"
+                    ll.add(map);
+                }
+
+                if(node.containsKey("title")) {
+                    type = "Movie";
+                    value = node.get("title").asString();
+                    graphNode = new GraphNode(id, value, type);
+                    HashMap<String, GraphItem> map = new HashMap();
+                    map.put("data", graphNode); // On package dans un élément "data"
+                    ll.add(map);
+                }
+            });
+
+            // On crée pour chaque relation un GraphEdge
+            path.relationships().forEach(r -> {
+                GraphEdge graphEdge = new GraphEdge(r.id(), r.startNodeId(), r.endNodeId(), "PLAYED_IN");
+                HashMap<String, GraphItem> map = new HashMap();
+                map.put("data", graphEdge); // On package dans un élément "data"
+                ll.add(map);
+            });
+        }
+
+        t.success();
+        return ll;
     }
 
     public static abstract class GraphItem {
