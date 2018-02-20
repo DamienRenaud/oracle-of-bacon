1678b52167195ea3ba7d7951f08c37134f67dbfb
Damien Renaud <renauddamien86@gmail.com>
2018-02-20 12:01:17 +0100

    Suppression du mock, appel de la méthode du repository neo4j et parsing du résultat en JSON

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
@@ -1,5 +1,6 @@
 package com.serli.oracle.of.bacon.api;
 
+import com.google.gson.Gson;
 import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
 import com.serli.oracle.of.bacon.repository.MongoDbRepository;
 import com.serli.oracle.of.bacon.repository.Neo4JRepository;
@@ -7,7 +8,9 @@ import com.serli.oracle.of.bacon.repository.RedisRepository;
 import net.codestory.http.annotations.Get;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class APIEndPoint {
@@ -26,45 +29,10 @@ public class APIEndPoint {
     @Get("bacon-to?actor=:actorName")
     public String getConnectionsToKevinBacon(String actorName) {
 
-        return "[\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 85449,\n" +
-                "\"type\": \"Actor\",\n" +
-                "\"value\": \"Bacon, Kevin (I)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 2278636,\n" +
-                "\"type\": \"Movie\",\n" +
-                "\"value\": \"Mystic River (2003)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 1394181,\n" +
-                "\"type\": \"Actor\",\n" +
-                "\"value\": \"Robbins, Tim (I)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 579848,\n" +
-                "\"source\": 85449,\n" +
-                "\"target\": 2278636,\n" +
-                "\"value\": \"PLAYED_IN\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 9985692,\n" +
-                "\"source\": 1394181,\n" +
-                "\"target\": 2278636,\n" +
-                "\"value\": \"PLAYED_IN\"\n" +
-                "}\n" +
-                "}\n" +
-                "]";
+        ArrayList ll = (ArrayList) neo4JRepository.getConnectionsToKevinBacon(actorName);
+        String json = new Gson().toJson(ll);
+        System.out.println(json);
+        return json;
     }
 
     @Get("suggest?q=:searchQuery")
